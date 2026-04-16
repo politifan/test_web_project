5895544a7328548efeea0517221edb10cc125077
StasShmel <stasshirobokov41@gmail.com>
2026-04-16 20:03:24 +0400

    Написал игру Угадай число

--- py.code/python.py
+++ py.code/python.py
@@ -1,5 +1,11 @@
-a = int(input())
-if a % 2 == 0:
-    print("Число четное");
-elif a % 2 != 0:
-    print("Число нечтное");
+import random
+a = random.randrange(100) + 1
+print("Давай мыграем в игру Угадай число")
+guest = int(input("Какое число я загадал ? "))
+while guest != a:
+    if guest > a:
+        print("Меньше...")
+    else:
+        print("Больше...")
+    guest = int(input("Подумай еще раз и напиши число : "))
+print ("Ты молодец! Я загадал число ", a)
